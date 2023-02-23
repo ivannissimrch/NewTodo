db904154c3a370c051f68349eca0e63cea484969
Ivan <ivannissimrch@gmail.com>
2023-02-22 19:29:20 -0500

    delete Todo  working working on edit Todo

--- app.js
+++ app.js
@@ -16,14 +16,29 @@ const markCompleted = (event) => {
   itemToMark.classList.add("completed");
 };
 
+const deleteToDo = (event) => {
+  const itemId = event.target.id.slice(3);
+  console.log(itemId);
+  const itemToDelete = document.getElementById(`container${itemId}`);
+  itemToDelete.remove();
+};
+
+const editToDo = (event) => {
+  const itemId = event.target.id.slice(4);
+  const itemToEdit = document.getElementById(itemId);
+  itemToEdit.readOnly = false;
+  itemToEdit.focus();
+  itemToEdit.classList.remove("completed");
+};
+
 const createToDo = (event) => {
   event.preventDefault();
   const results = document.getElementsByClassName("results-container")[0];
   let inputValue = document.getElementById("create-input");
-  results.innerHTML += `<div class="toDoListContainer">
+  results.innerHTML += `<div class="toDoListContainer" id=container${inputValue.value}>
         <input type="text" class="todoLabel" readonly  id=${inputValue.value} value=${inputValue.value}></input>
-        <button class="del-btn">del</button>
-         <button class="edit-btn">edit</button>
+        <button class="del-btn" id=del${inputValue.value}>del</button>
+         <button class="edit-btn" id=edit${inputValue.value}>edit</button>
           <button class="compl-btn" id=btn${inputValue.value}>compl</button>
 
     </div>`;
@@ -33,6 +48,19 @@ const createToDo = (event) => {
   completedBtnList.forEach((btn) => {
     btn.addEventListener("click", markCompleted);
   });
+
+  let deleteBtnList = document.getElementsByClassName("del-btn");
+  deleteBtnList = Array.from(deleteBtnList);
+  deleteBtnList.forEach((btn) => {
+    btn.addEventListener("click", deleteToDo);
+  });
+
+  let editBtnList = document.getElementsByClassName("edit-btn");
+  editBtnList = Array.from(editBtnList);
+  editBtnList.forEach((btn) => {
+    btn.addEventListener("click", editToDo);
+  });
+
   inputValue.value = "";
   inputValue.focus();
 };
